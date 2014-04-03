7e252e11237640481b41f62e0ee216c061bb77a1
Andrey Panteleyev <insonifi@gmail.com>
2014-04-04 02:29:33 +0400

    more comprehensive subscriber model

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var net = require('net'),
-  agent = require('webkit-devtools-agent'),
+//  agent = require('webkit-devtools-agent'),
   http = require('http'),
   messages = require('./lib/messages'),
   Conveyor = require('./lib/conveyor'),
   conveyor = new Conveyor;
 
+conveyor.onmsg({type: 'CAM'}, function (msg) {
+  //console.log('wtf!!!');
+})
+
+conveyor.onmsg({type: 'CAM', action: 'REC'}, function (msg) {
+  //console.log('recording?');
+})
+
+conveyor.onmsg({type: 'CORE'}, function (msg) {
+  //console.log(new Date);
+})
+t1 = new Date()
+for (var i = 10000; i--;) {
+  (function (i) {
+    conveyor.onmsg({type: 'CAM', id: i}, function (msg) {
+      //console.log('got cam', i);
+    })
+  }) (i)
+}
+console.log((new Date) - t1);
 console.log('pid', process.pid)
 /**** Server ****/
 server = net.createServer();
