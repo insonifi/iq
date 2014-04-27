4f7effb31a88ae041961024c913563daeecef1af
Andrey Panteleyev <insonifi@gmail.com>
2014-04-27 22:52:00 +0400

    fix client

--- test/listening.js
+++ test/listening.js
@@ -1,7 +1,6 @@
 var iq = require('../lib/iq');
 
 iq.listen('iidk');
-iq.on({type: 'CAM', id: '0'}, function () {
-  console.log(this);
+iq.on({type: 'CAM', action: 'REC'}, function () {
   iq.send(this);
 })
