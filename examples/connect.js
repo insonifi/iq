5b89a811c689a84e28f6da424ef205533c9eb7a8
Andrey Panteleyev <insonifi@gmail.com>
2014-05-18 00:38:50 +0400

    Introduce get CONFIG/STATE functions. Make use of promises. Update tests.

--- examples/connect.js
+++ examples/connect.js
@@ -1,6 +1,15 @@
 var iq = require('../lib/iq');
 
-iq.connect({ip: '127.0.0.1', iidk: '1'});
-iq.on({}, function () {
-  iq.DoReact(this);
-})
+iq.connect({ip: '127.0.0.1', iidk: '1', host: 'VM490'})
+.then(function () {
+  iq.on({type: 'MACRO'}, function (msg) {
+    console.log(msg.type, msg.action);
+    iq.get({type: 'CAM', prop: 'STATE'}).then(function (list) {
+      var i = 0,
+        len = list.length;
+      for (i = 0; i < len; i += 1) {
+        console.log('[state]', list[i].params.state);
+      }
+    });
+  });
+});
--- test/connection-test.js
+++ test/connection-test.js
@@ -1,17 +1,21 @@
-var iq = require('../lib/iq'),
+var iq_server = require('../lib/iq'),
+  iq_client = require('../lib/iq'),
   vows = require('vows'),
   assert = require('assert'),
-  a_message = {type: 'OBJECT', action: 'ACTION', id: '0'},
-  b_message = {type: 'OBJECT', action: 'ACTION', id: '1'},
-  state = {};
-  
+  i = 0;
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+});
+
 vows.describe('connection-test').addBatch({
   'Start listener': {
     topic: function () {
-      server = iq.listen('iidk');
-      return server;
+      iq_server.listen('iidk').then((function (socket) {
+        this.callback(null, socket)
+      }).bind(this));
     },
-    'Check socket': function (topic) {
+    'Socket open': function (topic) {
       var address = topic.address();
       assert.isObject(topic);
       assert.equal(address.address, '0.0.0.0');
@@ -20,50 +24,108 @@ vows.describe('connection-test').addBatch({
   },
   'Connect to listener': {
     topic: function () {
-      socket = iq.connect({port: 'iidk'});
-      return socket;
+      iq_client.connect({port: 'iidk'}).then((function (socket) {
+        this.callback(null, socket)
+      }).bind(this));
     },
-    'Check socket': function (topic) {
+    'Socket open': function (topic) {
       assert.isObject(topic);
       assert.equal(topic.remoteAddress, '127.0.0.1');
       assert.equal(topic.remotePort, '21030');
     }
   },
-  'Test communication': {
-    'DoReact': {
+  'Test communication:': {
+    'Send/receive reaction (Server -> Client)': {
       topic: function () {
-        var $this = this;
-        iq.on(a_message, function (msg) {
-          $this.callback(null, msg);
-        });
-        iq.DoReact(a_message);
+        var a_message = {type: 'OBJECT', action: 'ACTION', id: '0'};
+        iq_client.on(a_message, (function (msg) {
+          this.callback(null, msg);
+        }).bind(this));
+        iq_server.sendReact(a_message);
       },
-      'Compare sent/received message': function (err, msg) {
+      'Received reaction': function (err, msg) {
         assert.equal(msg.msg, 'React');
         assert.equal(msg.type, 'OBJECT');
         assert.equal(msg.action, 'ACTION');
         assert.equal(msg.id, '0');
-        assert.isObject(msg.params);
+        assert.include(msg, 'params');
       }
     },
-    'NotifyEvent': {
+    'Send/receive event (Client -> Server)': {
       topic: function () {
-        var $this = this;
-        iq.on(b_message, function (msg) {
-          $this.callback(null, msg);
-        });
-        iq.NotifyEvent(b_message);
+        var b_message = {type: 'OBJECT', action: 'ACTION', id: '1'},
+          $this = this;
+        iq_server.on(b_message, (function (msg) {
+          this.callback(null, msg);
+        }).bind(this));
+        iq_client.sendEvent(b_message);
+      },
+      'Received event': function (err, msg) {
+        assert.equal(msg.msg, 'Event');
+        assert.equal(msg.type, 'OBJECT');
+        assert.equal(msg.action, 'ACTION');
+        assert.equal(msg.id, '1');
+        assert.include(msg, 'params');
+      }
+    },
+    'Receive event from interface (Server -> Client)': {
+      topic: function () {
+        var iface_message = {type: 'ACTIVEX', action: 'EVENT', params: {objtype: 'OBJECT', objaction: 'ACTION', objid: '2'}},
+          subs_message = {type: 'OBJECT', action: 'ACTION', id: '2'};
+        iq_client.on(subs_message, (function (msg) {
+          this.callback(null, msg);
+        }).bind(this));
+        iq_server.sendEvent(iface_message);
       },
       'Compare sent/received message': function (err, msg) {
         assert.equal(msg.msg, 'Event');
         assert.equal(msg.type, 'OBJECT');
         assert.equal(msg.action, 'ACTION');
-        assert.equal(msg.id, '1');
-        assert.isObject(msg.params);
+        assert.equal(msg.id, '2');
+        assert.include(msg, 'params');
+      }
+    }
+  },
+  'API:': {
+    'Get config:': {
+      topic: function () {
+        iq_server.on({type: 'CORE', action: 'GET_CONFIG'}, (function (msg) {
+          if (msg.params.objtype !== undefined) {
+            iq_server.sendEvent({type: 'ACTIVEX', action: 'OBJECT_CONFIG', params: {objtype: msg.params.objtype, objid: '0'}});
+            iq_server.sendEvent({type: 'ACTIVEX', action: 'OBJECT_CONFIG', params: {objtype: msg.params.objtype, objid: '1'}});
+            iq_server.sendEvent({type: 'ACTIVEX', action: 'OBJECT_CONFIG', params: {objtype: msg.params.objtype, objid: '2'}});
+            iq_server.sendEvent({type: 'ACTIVEX', action: 'END_CONFIG'});
+          }
+        }).bind(this));
+        iq_client.get({type: 'CAM', prop: 'CONFIG'}).then((function (list) {
+          this.callback(null, list);
+        }).bind(this));
+      },
+      'Got list of 3': function (err, list) {
+        assert.lengthOf(list, 3);
+        assert.equal(list[0].type, 'CAM');
+        assert.equal(list[0].id, '0');
+      }
+    },
+    'Get state:': {
+      topic: function () {
+        iq_server.on({type: 'CORE', action: 'GET_STATE'}, (function (msg) {
+          if (msg.params.objtype !== undefined) {
+            iq_server.sendEvent({type: 'ACTIVEX', action: 'OBJECT_STATE', params: {objtype: msg.params.objtype, objid: '0'}});
+            iq_server.sendEvent({type: 'ACTIVEX', action: 'OBJECT_STATE', params: {objtype: msg.params.objtype, objid: '1'}});
+            iq_server.sendEvent({type: 'ACTIVEX', action: 'OBJECT_STATE', params: {objtype: msg.params.objtype, objid: '2'}});
+            iq_server.sendEvent({type: 'ACTIVEX', action: 'END_STATE'});
+          }
+        }).bind(this));
+        iq_client.get({type: 'CAM', prop: 'STATE'}).then((function (list) {
+          this.callback(null, list);
+        }).bind(this));
+      },
+      'Got list of 3': function (err, list) {
+        assert.lengthOf(list, 3);
+        assert.equal(list[0].type, 'CAM');
+        assert.equal(list[0].id, '0');
       }
     }
   }
 }).export(module);
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
-});
