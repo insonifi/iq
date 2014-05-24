a09bb1ae3f121491863a1e8861bf9a91509c9472
Andrey Panteleyev <insonifi@gmail.com>
2014-05-24 16:39:07 +0400

    fix duplicate callback invocation

--- examples/listen.js
+++ examples/listen.js
@@ -1,6 +1,6 @@
 var iq = require('../lib/iq');
 
 iq.listen('iidk');
-iq.on({type: 'CAM', action: 'REC'}, function () {
-  iq.NotifyEvent(this);
+iq.on({}, function (msg) {
+  iq.sendEvent(msg);
 })
