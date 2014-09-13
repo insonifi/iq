ee84b95a52640876da5d57ffcd5c0f723114401b
Andrey Panteleyev <insonifi@gmail.com>
2014-09-14 00:08:05 +0400

    fix conversions and use async processing

--- examples/connect.js
+++ examples/connect.js
@@ -1,31 +1,17 @@
 var iq = require('../lib/iq'),
     catchErr = function (e) { console.log(e); };
 
-iq.connect({ip: '127.0.0.1', iidk: '1', host: 'VM490'})
-.then(function () {
-  iq.on({}, function (msg) {
-    iq.sendEvent(msg);
-  })
-}, catchErr)
-.then(function () {
-  iq.sendEvent({
-    type: 'WHAT',
-    id: 'THE',
-    action: 'FUCK',
-    params: {
-      param: 0
-    }
-  });
-}, catchErr)
+iq.connect({ip: '192.168.122.183', iidk: '3', host: 'DUKE-PC'})
 .then(function () {
   iq.on({type: 'MACRO'}, function (msg) {
     console.log(msg.type, msg.action);
-    iq.get({type: 'CAM', prop: 'STATE'}).then(function (list) {
-      var i = 0,
-        len = list.length;
-      for (i = 0; i < len; i += 1) {
-        console.log('[state]', list[i].params.state);
-      }
-    });
+      iq.sendEvent({
+        type: 'WHAT',
+        id: 'THE',
+        action: 'FUCK',
+        params: {
+          param: 0
+        }
+      });
   });
-});
+}, catchErr);
