9847aa20a455e278a36894250b89eebcfff75302
Andrey <insonifi@gmail.com>
2015-01-02 16:26:00 +0300

    Update connect.js

--- examples/connect.js
+++ examples/connect.js
@@ -1,17 +1,6 @@
 var iq = require('../lib/iq'),
+    doSomething = function () {},
     catchErr = function (e) { console.log(e); };
 
-iq.connect({ip: '192.168.122.183', iidk: '3', host: 'DUKE-PC'})
-.then(function () {
-  iq.on({type: 'MACRO'}, function (msg) {
-    console.log(msg.type, msg.action);
-      iq.sendEvent({
-        type: 'WHAT',
-        id: 'THE',
-        action: 'FUCK',
-        params: {
-          param: 0
-        }
-      });
-  });
-}, catchErr);
+iq.connect({ip: '127.0.0.1', iidk: '1', host: 'SERVER'})
+.then(doSomething, catchErr);
